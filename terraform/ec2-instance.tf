6d89f42e9fe44039fb3bd281fc9c40cac8293214
Faraz Foroozan <farazforoozan@gmail.com>
2021-07-10 12:02:40 +0200

    Added elastic ip

--- terraform/ec2-instance.tf
+++ terraform/ec2-instance.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "my-ec2-vm" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.ec2_instance_type
   key_name               = "private"
-  count                  = 1
+  # count                  = 1
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
-    "Name" = "ubuntu-server-${count.index}"
+    "Name" = "web-server-1"
   }
 }
